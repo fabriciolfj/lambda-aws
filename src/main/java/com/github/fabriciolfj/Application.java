2d73d141ae30696729c8d7f429a1d6a82ef81e46
fabricio <fabricio.jacob@outlook.com>
2021-08-19 10:14:21 -0300

    add

--- src/main/java/com/github/fabriciolfj/Application.java
+++ src/main/java/com/github/fabriciolfj/Application.java
@@ -1,9 +1,7 @@
 package com.github.fabriciolfj;
 
 import com.github.fabriciolfj.business.usercase.ProductCase;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class Application {
 
     public static void main(String[] args) {
